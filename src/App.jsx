09767791123913156f47aafa250a59b7231d2811
walidhassan1011 <walidhassan009@gmail.com>
2022-08-23 23:00:15 +0200

    video ui

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import styled, { ThemeProvider } from "styled-components";
+import Menu from "./components/Menu";
+import Navbar from "./components/Navbar";
+import { darkTheme, lightTheme } from "./utils/Theme";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Video from "./pages/Video";
+const Container = styled.div`
+  display: flex;
+`;
+
+const Main = styled.div`
+  flex: 7;
+  background-color: ${({ theme }) => theme.bg}; ;
+`;
+const Wrapper = styled.div`
+  padding: 22px 96px;
+`;
+function App() {
+  const [theme, setTheme] = useState(true);
+  return (
+    <ThemeProvider theme={theme ? darkTheme : lightTheme}>
+      <Container>
+        <Router>
+          <Menu theme={theme} setTheme={setTheme} />
+          <Main>
+            <Navbar />
+            <Wrapper>
+              <Routes>
+                <Route path="/">
+                  <Route index element={<Home />} />
+                  <Route path="video">
+                    <Route path=":id" element={<Video />} />
+                  </Route>
+                </Route>
+              </Routes>
+            </Wrapper>
+          </Main>
+        </Router>
+      </Container>
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ src/components/Card.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 360px;
+  margin-bottom: 45px;
+  cursor: pointer;
+`;
+
+const Img = styled.img`
+  width: 100%;
+  height: 202px;
+  background-color: #999;
+`;
+const Details = styled.div`
+  display: flex;
+  margin-top: 16px;
+  gap: 12px;
+`;
+const ChannelImg = styled.img`
+  width: 36px;
+  height: 36px;
+  border-radius: 50%;
+  background-color: #999;
+`;
+const Texts = styled.div``;
+const ChannelName = styled.h2`
+  font-size: 14px;
+  color: ${({ theme }) => theme.textSoft};
+  margin: 9px 0px;
+`;
+const Title = styled.h1`
+  font-size: 16px;
+  font-weight: 500;
+  color: ${({ theme }) => theme.text};
+`;
+const Info = styled.div`
+  font-size: 14px;
+  color: ${({ theme }) => theme.textSoft};
+`;
+function Card() {
+  return (
+    <Container>
+      <Img src="https://i9.ytimg.com/vi_webp/k3Vfj-e1Ma4/mqdefault.webp?v=6277c159&sqp=CIjm8JUG&rs=AOn4CLDeKmf_vlMC1q9RBEZu-XQApzm6sA" />
+      <Details>
+        <ChannelImg src="https://yt3.ggpht.com/yti/APfAmoE-Q0ZLJ4vk3vqmV4Kwp0sbrjxLyB8Q4ZgNsiRH=s88-c-k-c0x00ffffff-no-rj-mo" />
+        <Texts>
+          <Title>Test Video</Title>
+          <ChannelName>Willy dev</ChannelName>
+          <Info>66,000 views • 2 years ago</Info>
+        </Texts>
+      </Details>
+    </Container>
+  );
+}
+
+export default Card;
